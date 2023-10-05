6b553cd444e9c3f0a38f3aecd3191f88c5bc4ab4
Mohammad Siddiqui <masiddiqui91@gmail.com>
2023-10-05 16:00:01 +0100

    layout with sidebar

--- src/_example/layouts/BaseLayout.vue
+++ src/_example/layouts/BaseLayout.vue
@@ -2,9 +2,9 @@
   <div>
     <SideBar
       :class="{ '-translate-x-full': !openSideBar, 'translate-x-0': openSideBar }"
-      class="fixed top-0 left-0 z-20 h-full pb-10 overflow-x-hidden overflow-y-auto transition origin-left transform bg-white border-r w-60 lg:translate-x-0"
+      class="fixed top-0 left-0 z-20 h-full overflow-x-hidden overflow-y-auto transition origin-left transform w-60 shadow-lg"
     />
-    <div class="ml-0 transition lg:ml-60">
+    <div class="transition" :class="mainContentClass">
       <SAppbar title="MASC Components" class="shadow-sm">
         <template #toggle>
           <SBtn class="btn-ghost text-2xl btn-circle" icon="mdi:menu" @click="openSideBar = !openSideBar" />
@@ -25,6 +25,13 @@
 
 <script setup lang="ts">
 import SideBar from "./SideBar.vue";
-import { ref } from "vue";
-const openSideBar = ref(false);
+import { ref, computed } from "vue";
+import { useWindowSize } from "@vueuse/core";
+const { width, height } = useWindowSize();
+const openSideBar = ref(true);
+
+const mainContentClass = computed(() => {
+  if (width.value < 1024) return "ml-0";
+  return openSideBar.value ? "ml-60" : "ml-0";
+});
 </script>
--- src/_example/layouts/SideBar.vue
+++ src/_example/layouts/SideBar.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="w-full bg-gray-700 text-white">
+  <div class="w-full bg-gray-700 text-white h-screen flex flex-col">
     <h1 class="text-xl font-bold p-4">MASC Components</h1>
-    <ul class="text-gray-50">
-      <li v-for="x in 10" class="">
+    <ul class="text-gray-50 overflow-y-auto flex-1 h-full">
+      <li v-for="x in 20" class="border-b border-gray-500">
         <a href="#" class="block px-4 py-2">Side Item {{ x }}</a>
       </li>
     </ul>
