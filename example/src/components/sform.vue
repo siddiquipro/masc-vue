bde799495f4e1269cc1e73434c885412f432b282
Mohammad Siddiqui <masiddiqui91@gmail.com>
2023-11-21 13:05:25 +0000

    s field changes

--- example/src/components/sform.vue
+++ example/src/components/sform.vue
@@ -1,24 +1,24 @@
 <template>
-  <SCard title="Form" class="relative">
-    <sForm class="grid gap-3" :data="ds.obj" v-slot="{ clearError }" @submit="onSubmit">
-      <div v-for="col in config.columns">
-        <sField
-          :label="col.label"
-          :type="col.type"
-          :help="col.help"
-          :prop="col.field"
-          :options="col.opts"
-          :required="col.required"
-          v-model="ds.obj[col.field]"
-        />
-      </div>
+	<SCard title="Form" class="relative">
+		<sForm class="grid gap-3" :data="ds.obj" v-slot="{ clearError }" @submit="onSubmit">
+			<div v-for="col in config.columns">
+				<sField
+					:label="col.label"
+					:type="col.type"
+					:help="col.help"
+					:prop="col.field"
+					:options="col.opts"
+					:required="col.required"
+					v-model="ds.obj[col.field]"
+				/>
+			</div>
 
-      <div class="flex gap-4 mt-4">
-        <s-btn type="submit" class="px-10 btn-primary">Submit</s-btn>
-        <s-btn type="button" class="px-10 btn-ghost" @click="clearError()">Clear</s-btn>
-      </div>
-    </sForm>
-  </SCard>
+			<div class="flex gap-4 mt-4">
+				<s-btn type="submit" class="px-10 btn-primary">Submit</s-btn>
+				<s-btn type="button" class="px-10 btn-ghost" @click="clearError()">Clear</s-btn>
+			</div>
+		</sForm>
+	</SCard>
 </template>
 
 <script lang="ts" setup>
@@ -30,74 +30,80 @@ const notify = useNotify();
 const ds: any = reactive({ obj: {} });
 
 const config = {
-  columns: [
-    {
-      field: "texta",
-      help: "Please provide something useful",
-      label: "Your Name",
-      required: true,
-      type: "text",
-    },
-    {
-      field: "email",
-      help: "Please provide your email address",
-      label: "Your Email ",
-      required: true,
-      type: "email",
-    },
-    // {
-    //   field: "search",
-    //   label: "This is label for type SEARCH",
-    //   type: "search",
-    // },
-    {
-      field: "textareab",
-      label: "This is label for type TEXTAREA",
-      type: "textarea",
-    },
+	columns: [
+		{
+			field: "texta",
+			help: "Please provide something useful",
+			label: "Your Name",
+			required: true,
+			type: "text",
+		},
+		{
+			field: "email",
+			help: "Please provide your email address",
+			label: "Your Email ",
+			required: true,
+			type: "email",
+		},
+		// {
+		//   field: "search",
+		//   label: "This is label for type SEARCH",
+		//   type: "search",
+		// },
+		{
+			field: "textareab",
+			label: "This is label for type TEXTAREA",
+			type: "textarea",
+		},
 
-    {
-      field: "dated",
-      label: "Your date of birth",
-      type: "date",
-    },
+		{
+			field: "dated",
+			label: "Your date of birth",
+			type: "date",
+		},
 
-    {
-      field: "select",
-      label: "Are you sure?",
-      required: true,
-      type: "select",
-      opts: [
-        { text: "Yes", value: "yes" },
-        { text: "No", value: "no" },
-        { text: "May Be", value: "maybe" },
-      ],
-    },
-    {
-      field: "radioc",
-      label: "Confirm your choice",
-      type: "radio",
-      opts: [
-        { text: "Yes", value: "yes" },
-        { text: "No", value: "no" },
-      ],
-    },
-    {
-      field: "checkbox",
-      label: "Confirm your choices",
-      type: "checkbox",
-      opts: [
-        { text: "Yes", value: "yes" },
-        { text: "No", value: "no" },
-        { text: "May Be", value: "maybe" },
-      ],
-    },
-  ],
+		{
+			field: "select",
+			label: "Are you sure?",
+			required: true,
+			type: "select",
+			opts: [
+				{ text: "Yes", value: "yes" },
+				{ text: "No", value: "no" },
+				{ text: "May Be", value: "maybe" },
+			],
+		},
+		{
+			field: "radioc",
+			label: "Confirm your choice",
+			type: "radio",
+			opts: [
+				{ text: "Yes", value: "yes" },
+				{ text: "No", value: "no" },
+			],
+		},
+		{
+			field: "checkbox",
+			label: "Confirm your choices",
+			type: "checkbox",
+			opts: [
+				{ text: "Yes", value: "yes" },
+				{ text: "No", value: "no" },
+				{ text: "May Be", value: "maybe" },
+			],
+		},
+
+		{
+			field: "file",
+			label: "Choose File",
+			type: "file",
+		},
+	],
 };
 
 function onSubmit(data: any) {
-  console.log("Form submitted", data);
-  notify.success("Form submitted");
+	console.log("Form submitted", data);
+	notify.success("Form submitted");
 }
 
 // ordering of cols
--- packages/masc/src/components/formitem/s-file-field.vue
+++ packages/masc/src/components/formitem/s-file-field.vue
@@ -1,13 +1,19 @@
 <template>
-	<div class="rounded input h-auto w1">
-		<label class="flex items-center justify-center flex-col cursor-pointer">
-			<input class="file-input opacity-0 absolute top-0" type="file" name="file" :required="required" @change="onFileChange" />
-			<div class="w-full flex flex-col items-center py-8" v-if="value">
-				<div>{{ value.name }}</div>
-			</div>
-			<div v-else class="w-full flex flex-col items-center py-4">
-				<Icon icon="mdi:upload" height="32" class="text-3xl" />
-				<span class="file-label"> Choose a file… </span>
+	<div class="rounded w1">
+		<label class="block cursor-pointer relative input" :class="getClasses">
+			<input
+				class="file-input opacity-0 absolute top-0 w-full z-0 cursor-pointer"
+				type="file"
+				name="file"
+				:required="required"
+				:readonly="readonly"
+				@change="onFileChange"
+			/>
+
+			<div class="w-full flex gap-2 items-center py-2">
+				<Icon icon="mdi:upload" />
+				<span v-if="value">{{ value.name }}</span>
+				<span v-else class="file-label"> Choose a file… </span>
 			</div>
 		</label>
 	</div>
@@ -16,7 +22,7 @@
 <script setup lang="ts">
 import { useVModel } from "@vueuse/core";
 import Icon from "../icon/icon.vue";
-import { PropType } from "vue";
+import { PropType, computed } from "vue";
 
 const props = defineProps({
 	modelValue: {
@@ -27,6 +33,23 @@ const props = defineProps({
 		type: Boolean,
 		default: false,
 	},
+	readonly: {
+		type: Boolean,
+		default: false,
+	},
+
+	size: {
+		type: String as PropType<"xs" | "sm" | "lg">,
+		default: "",
+	},
+});
+
+const getClasses = computed(() => {
+	const cls = [];
+	if (props.size === "xs") cls.push("input-xs");
+	if (props.size === "sm") cls.push("input-sm");
+	if (props.size === "lg") cls.push("input-lg");
+	return cls.join(" ");
 });
 
 const value = useVModel(props, "modelValue");
